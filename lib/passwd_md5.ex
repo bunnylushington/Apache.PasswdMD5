5ddd6d935d52a0df73327fc28a2e3ab5e4085624
Kevin Montuori <montuori@gmail.com>
2014-09-21 10:05:25 -0400

    added salt extraction and generation

--- lib/passwd_md5.ex
+++ lib/passwd_md5.ex
@@ -1,2 +1,50 @@
 defmodule PasswdMD5 do
+
+  @magic_md5 "$1$"
+  @magic_apr "$apr1$"
+  @atoz  "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+  def apache_md5_crypt(pw, salt), do: unix_md5_crypt(pw, salt, "$apr1$")
+
+  def unix_md5_crypt(pw, salt, magic \\ "$1$") do
+  end
+
+
+  def extract_salt(str) do
+    {_, salt, _} = parse_hash(str)
+    if salt == nil, do: {:error, nil}, else: {:ok, salt}
+  end
+
+  def extract_salt!(str) do
+    case extract_salt(str) do
+      {:ok, salt} -> salt
+      {:error, nil} -> raise "Valid salt not found in string #{ str }."
+    end
+  end
+
+  def extract_or_generate_salt(str) do
+    case extract_salt(str) do
+      {:ok, salt} -> salt
+      _ -> generate_salt
+    end
+  end
+
+  defp generate_salt(length \\ 8, seed \\ :os.timestamp) do
+    :random.seed(seed)
+    len = String.length(@atoz)
+    chr = for _ <- 1 .. length, do: String.at(@atoz, (:random.uniform(len) - 1))
+    Enum.join chr
+  end
+
+  defp parse_hash(str) do
+    magic_part = "(#{ Regex.escape @magic_md5 }|#{ Regex.escape @magic_apr })"
+    salt_part = "([^\\$]{0,8})\\$"
+    regex = Regex.compile! "^#{magic_part}#{salt_part}(.+)$"
+    case Regex.run(regex, str, capture: :all_but_first) do
+      [@magic_md5, salt, crypted_pw] -> {:md5, salt, crypted_pw}
+      [@magic_apr, salt, crypted_pw] -> {:apr, salt, crypted_pw}
+      _                              -> {:invalid_hash, nil, nil}
+    end
+  end
+
 end
--- test/passwd_md5_test.exs
+++ test/passwd_md5_test.exs
@@ -1,7 +1,31 @@
 defmodule PasswdMD5Test do
   use ExUnit.Case
+  alias PasswdMD5, as: MD
+
+  @salt "01234567"
+  @pass "password"
+  @md5  "$1$01234567$b5lh2mHyD2PdJjFfALlEz1"
+  @apr  "$apr1$01234567$IXBaQywhAhc0d75ZbaSDp/"
+
 
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "extract salt" do
+    assert MD.extract_salt!(@apr) == @salt
+    assert MD.extract_salt!(@md5) == @salt
+    assert_raise RuntimeError, fn -> MD.extract_salt!("random string") end
+    assert {:ok, @salt} = MD.extract_salt(@apr)
+    assert {:ok, @salt} = MD.extract_salt(@md5)
+    assert {:error, nil} = MD.extract_salt("random string")
+  end
+
+  test "extract or generate salt" do
+    assert MD.extract_or_generate_salt(@apr) == @salt
+    assert MD.extract_or_generate_salt(@md5) == @salt
+    assert String.length(MD.extract_or_generate_salt("random string")) == 8
+  end
+
+
 end
