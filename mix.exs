ba2bad1a8ce51c41fcd6a77b36d9e38edba0ae47
Kevin Montuori <montuori@gmail.com>
2014-09-27 12:46:58 -0500

    added hex.pm ifo

--- mix.exs
+++ mix.exs
@@ -3,9 +3,26 @@ defmodule PasswdMD5.Mixfile do
 
   def project do
     [app: :passwd_md5,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.0.0-rc1",
+     description: description,
+     package: package,
      deps: deps]
+  end
+
+  def description do
+    """
+    Provides Apache/APR style password hashing.  Useful for generating or 
+    authenticating against MD5 htpasswd passwords.
+    """
+  end
+  
+  def package do
+    [
+     files: ["lib", "mix.exs", "README*", "test"],
+     contributors: ["Kevin Montuori"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/kevinmontuori/Apache.PasswdMD5"}]
   end
 
   def application do
