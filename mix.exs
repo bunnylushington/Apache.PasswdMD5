bbb8fd49a28c601308a999f789b302529ffea543
Kevin Montuori <montuori@gmail.com>
2014-09-27 12:52:54 -0500

    heh.  changed name to match what mix allows.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PasswdMD5.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :Apache_PasswdMD5,
+    [app: :apache_passwd_md5,
      version: "1.0.0",
      elixir: "~> 1.0.0-rc1",
      description: description,
